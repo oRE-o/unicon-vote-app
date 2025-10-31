a34ee21c0e25e4beb5a70a1e8a0a8d0baf92ee73
ore-o <cyhs9120@naver.com>
2025-10-31 13:56:23 +0900

    asdf

--- unicon-vote-frontend/src/components/GameCard.tsx
+++ unicon-vote-frontend/src/components/GameCard.tsx
@@ -47,7 +47,7 @@ function GameCard({
         <img
           src={game.imageUrl}
           alt={game.name}
-          className="h-56 w-full object-cover"
+          className="h-56 w-full object-contain"
         />
       </figure>
       <div className="card-body flex-grow">
